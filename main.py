59ca9ae6e80b15dcaf3f9aa3d7e227661e71ab6b
Andrew Preston <39848684+LMDZDrew@users.noreply.github.com>
2026-09-04 23:15:19 -0400

    Add input validation

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-budget = float(input("What is your PC budget? $"))
+while True:
+    try:
+        budget = float(input("What is your PC budget? $"))
+        break
+    except ValueError:
+        print("Please enter a valid number.")
 
 parts = {}
 
@@ -13,8 +18,13 @@ component_names = [
 ]
 
 for component in component_names:
-    price = float(input(f"How much does your {component} cost? $"))
-    parts[component] = price
+    while True:
+        try:
+            price = float(input(f"How much does your {component} cost? $"))
+            parts[component] = price
+            break
+        except ValueError:
+            print("Please enter a valid number.")
 
 total_cost = sum(parts.values())
 remaining_budget = budget - total_cost
