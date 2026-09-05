45580d64a1f70cf71bdbfaf24af855b6529138f4
Andrew Preston <39848684+LMDZDrew@users.noreply.github.com>
2026-09-04 22:53:07 -0400

    Add initial PC budget calculator

---
+++ main.py
@@ -0,0 +1,20 @@
+budget = float(input("What is your PC budget? $"))
+cpu = float(input("How much does your CPU cost? $"))
+gpu = float(input("How much does your GPU cost? $"))
+ram = float(input("How much does your RAM cost? $"))
+motherboard = float(input("How much does your Motherboard cost? $"))
+storage = float(input("How much does your Storage cost? $"))
+power_supply = float(input("How much does your Power Supply cost? $"))
+case = float(input("How much does your Case cost? $"))
+remaining_budget = budget - cpu - gpu - ram - motherboard - storage - power_supply - case
+
+print(f"Remaining Budget: ${remaining_budget:.2f}")
+
+if remaining_budget > 0:
+    print(f"You are within budget with ${remaining_budget:.2f} remaining!")
+
+elif remaining_budget == 0:
+    print("You used your entire budget!")
+
+else:
+    print(f"You are over budget by ${abs(remaining_budget):.2f}!")
